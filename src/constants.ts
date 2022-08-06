d0935b7d652a1797650526a768a87856f10a4df3
Nayan Patil <patil.nayan04@gmail.com>
2022-08-06 11:00:03 +0530

    initial setup

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const COOKIE_NAME = process.env.COOKIE_NAME
+export const __prod__ = process.env.NODE_ENV === 'production'
+export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { Request, Response } from "express";
+import cors from "cors"
+import session from "express-session"
+import { COOKIE_NAME, __prod__ } from "./constants";
+
+dotenv.config();
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    }),
+  );
+
+  app.set('trust proxy', 1);
+  app.use(express.static('public'));
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: 'none', //CSRF
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET_KEY,
+      resave: false,
+    }),
+  );
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port", process.env.PORT);
+});
