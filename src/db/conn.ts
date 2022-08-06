ae18b1139aaefba62c9c4ca102cc72b05bb89aff
Nayan Patil <patil.nayan04@gmail.com>
2022-08-06 20:00:34 +0530

    database setup

---
+++ src/db/conn.ts
@@ -0,0 +1,39 @@
+import Mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const initdb = async () => {
+  if (Mongoose.connections[0].readyState) {
+    console.log("Already Connected");
+    return;
+  }
+
+  try {
+      await Mongoose.connect(process.env.DATABASE_URL);
+      console.log(Mongoose.connections[0].readyState);
+
+    // await mongoose.connect('mongodb://localhost:27017/test');
+  } catch (error) {
+    console.log(error);
+  }
+
+  Mongoose.connection.on("connected", () => {
+    console.log("connected to mongoDB");
+  });
+  Mongoose.connection.on("error", (err) => {
+    console.log("err", err);
+  });
+};
+
+export default initdb;
+
+// import path from "path";
+// import { DataSource } from "typeorm";
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     url: process.env.DATABASE_URL,
+//     logging: true,
+//     // synchronize: true,
+//     migrations: [path.join(__dirname, "./migrations/*")],
+//     entities: ["dist/src/entities/*.entity.js"],
+//   });
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { Request, Response } from "express";
-import cors from "cors"
-import session from "express-session"
+import cors from "cors";
+import session from "express-session";
 import { COOKIE_NAME, __prod__ } from "./constants";
+import initdb from "./db/conn";
 
 dotenv.config();
 
@@ -16,29 +17,29 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    }),
-  );
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-  app.set('trust proxy', 1);
-  app.use(express.static('public'));
+app.set("trust proxy", 1);
+initdb();
 
-  app.use(
-    session({
-      name: COOKIE_NAME,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-        httpOnly: true,
-        sameSite: 'none', //CSRF
-        secure: __prod__,
-      },
-      saveUninitialized: false,
-      secret: process.env.SESSION_SECRET_KEY,
-      resave: false,
-    }),
-  );
+app.use(
+  session({
+    name: COOKIE_NAME,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+      httpOnly: true,
+      sameSite: "none", //CSRF
+      secure: __prod__,
+    },
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port", process.env.PORT);
